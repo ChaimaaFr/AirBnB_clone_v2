8fefb59552751b9af1809531eb91d05ceab961fb
ChaimaaFr <chaimaefarkouchi@gmail.com>
2023-10-22 16:28:52 -0700

    Odd or even

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
-""" 7. Start flask service that does something. """
-
-from flask import Flask
-from flask import render_template
-
+""" Starts a Flask web app """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-#@app.route()
+@app.teardown_appcontext
+def dispose(exception):
+    """ Remove current session """
+    storage.close()
+
+
+@app.route('/states_list')
+def states():
+    """ Display list of all the states """
+    states = storage.all(State)
+    states_list = list(states.values())
+    return render_template('7-states_list.html', states=states_list)
 
 
 if __name__ == '__main__':
